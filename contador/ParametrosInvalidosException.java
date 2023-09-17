2a385aabfd1c098a59f6f3a25964f7edfeedebf5
Deyky Pinheiro <pinheiromikael96@gmail.com>
2023-09-17 15:18:36 -0300

    feat: implemented counter

---
+++ contador/ParametrosInvalidosException.java
@@ -0,0 +1,7 @@
+package contador;
+
+public class ParametrosInvalidosException extends RuntimeException {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
